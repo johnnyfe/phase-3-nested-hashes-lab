f6b0d644e8d7e56765ac584536e226fbe6a8c0dd
johnny <johnny1194@hotmail.com>
2021-07-05 02:50:25 -0400

    Completed assignment

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages].join
 
 end
 
@@ -82,6 +84,11 @@ def adding_matz
       }
     }
 
+    programmer_hash = {:yukihiro_matsumoto =>{
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }}
+
 end
 
 def changing_alan
@@ -104,6 +111,9 @@ def changing_alan
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]="GUI"
+    programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +135,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    programmer_hash
 
 end
